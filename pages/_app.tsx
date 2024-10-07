07427a2d0d2622797466e46f54a1f01000bc8148
Daniel PC <abracadaniel64@gmail.com>
2024-10-06 22:55:33 -0400

    tried to change title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../app/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <Head>
+      <title>Explain Like I'm 5</title>
+      <link rel="icon" href="/png/smiling-face.png" type="image/png" />
+    </Head>
+    <Component {...pageProps} />;
+  </>
+    
+)
 }
